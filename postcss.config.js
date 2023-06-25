8294f57c00e46bfed5caea24f5c19f8dfcb379db
jeremy.jumpertz@gmail.com <jeremy.jumpertz@gmail.com>
2023-06-25 23:06:27 +0200

    addin tailwindcss and i thing it work

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-export default {
+module.exports = {
   plugins: {
-    tailwindcss: {"./tailwind.config.js"},
+    tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   esbuild: {
@@ -10,6 +13,9 @@ export default defineConfig({
   css: {
     modules: {
       localsConvention: "camelCaseOnly",
+    },
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
     },
   },
 });
