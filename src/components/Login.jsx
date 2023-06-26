54fc1b49d03a30493d183635f60ca62813c5bdbd
jeremy.jumpertz@gmail.com <jeremy.jumpertz@gmail.com>
2023-06-26 19:49:57 +0200

    adding axios and request for Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
+import API from "../api";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await API.post("http://localhost:5000/login", {
+        email: email,
+        password: password,
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="bg-white white:bg-gray-900">
       <div className="flex justify-center h-screen">
@@ -26,6 +44,9 @@ function Login() {
                     name="email"
                     id="email"
                     placeholder="exemple@exemple.com"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    autoComplete="current-password"
                     className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg white:placeholder-gray-600 white:bg-gray-900 white:text-gray-300 white:border-gray-700 focus:border-blue-400 white:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                   />
                 </div>
@@ -51,12 +72,17 @@ function Login() {
                     name="password"
                     id="password"
                     placeholder="Votre mot de passe"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                     className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg white:placeholder-gray-600 white:bg-gray-900 white:text-gray-300 white:border-gray-700 focus:border-blue-400 white:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                   />
                 </div>
 
                 <div className="mt-6">
-                  <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                  <button
+                    onClick={handleSubmit}
+                    className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+                  >
                     Se connecter
                   </button>
                 </div>
